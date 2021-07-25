c47589514514cf44b95768611b39dbb3a0a19080
Elliott Arnold <si3mshady@Elliotts-MBP.attlocal.net>
2021-07-25 13:39:53 -0500

    done with f/e tutorial

--- src/components/topbar/TopBar.js
+++ src/components/topbar/TopBar.js
@@ -1,9 +1,11 @@
+import { Link } from 'react-router-dom'
 import './topbar.css'
 
 export default function TopBar() {
    const imgUrl =  "https://cdn.vox-cdn.com/thumbor/K1WKyMb31K-K1vvseGAyFsjfYYE=/0x0:1200x675/1200x800/filters:focal(478x31:670x223)/cdn.vox-cdn.com/uploads/chorus_image/image/60384393/0_c9S8ajFBpwX89ZuU.0.jpeg"
-
+   const user =   false;
     return (
+        
         <div className="top">
             <div className='topLeft'>
                 <i className="topIcon fab fa-facebook"></i>
@@ -12,16 +14,39 @@ export default function TopBar() {
             </div>
             <div className='topCenter'>
                 <ul className="topList">
-                    <li className="topListItem">HOME</li>
-                    <li className="topListItem">ABOUT</li>
-                    <li className="topListItem">CONTACT</li>
-                    <li className="topListItem">WRITE</li>
-                    <li className="topListItem">LOGOUT</li>
+                    <li className="topListItem">
+                        <Link className="link" to="/">HOME</Link>
+                    </li>
+                    <li className="topListItem">
+                    <Link className="link" to="/">ABOUT</Link>
+                    </li>
+                    <li className="topListItem">
+                    <Link className="link" to="/">CONTACT</Link>
+                    </li>
+                    <li className="topListItem">
+                    <Link className="link" to="/write">WRITE</Link>
+                    </li>
+                    <li className="topListItem">
+                    {user && <Link className="link" to="/">LOGOUT</Link>}
+                    </li>
 
                 </ul>
             </div>
             <div className='topRight'>
-                <img className="topImage" src={imgUrl} alt="blank"/>
+               { user ? (<img className="topImage" src={imgUrl} alt="blank"/>):
+               (
+               <ul className="topList">
+                   <li className="topListItem" >
+                    <Link to="/login" className="link"> LOGIN</Link>
+                   </li>
+                
+                   <li className="topListItem" > 
+                    <Link  to="/register"  className="link"> REGISTER</Link>
+                   </li>
+                
+               </ul>
+               ) }
+
                 <i class="topSearchIcon fas fa-search"></i>
             </div>            
         </div>
