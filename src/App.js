d764e372564f98137270750310ccca50886f2db4
Elliott Arnold <si3mshady@Elliotts-MBP.attlocal.net>
2021-07-25 20:53:55 -0500

    updated dockerfile + nginx.conf

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import Topbar from "./components/topbar/Topbar";
-import Homepage from "./pages/homepage/Homepage";
-import Login from "./pages/login/Login";
-import Register from "./pages/register/Register";
-import Settings from "./pages/settings/Settings";
-import Single from "./pages/single/Single";
-import Write from "./pages/write/Write";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import TopBar from './components/topbar/TopBar'
+import Homepage from './pages/home/Home'
+import Single from './pages/single/Single'
+import Write from './pages/write/Write'
+import Settings from './pages/settings/Settings'
+import Login from './pages/login/Login'
+import Register from './pages/register/Register'
+import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   const currentUser = true;
   return (
     <Router>
-      <Topbar />
+      <TopBar />
       <Switch>
         <Route exact path="/">
           <Homepage />
