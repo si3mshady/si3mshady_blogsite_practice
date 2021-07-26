80f1b7bffe09c8d0ac5f53fb67da5163d295c901
Elliott Arnold <si3mshady@Elliotts-MBP.attlocal.net>
2021-07-25 20:50:09 -0500

    updated dockerfile + nginx.conf

--- src/App.js
+++ src/App.js
@@ -1,60 +1,38 @@
-import TopBar from './components/topbar/TopBar'
-import HomePage from './pages/home/Home'
-import Single from './pages/single/Single'
-import Write from './pages/write/Write'
-import Settings from './pages/settings/Settings'
-import Login from './pages/login/Login'
-import Register from './pages/register/Register'
-import './App.css';
-
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import Topbar from "./components/topbar/Topbar";
+import Homepage from "./pages/homepage/Homepage";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Settings from "./pages/settings/Settings";
+import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  const user = true;
+  const currentUser = true;
   return (
-    <Router> 
-        <TopBar />  
-          <Switch>
-          <Route exact path="/">
-            <HomePage />
-          </Route>
-
-          <Route exact path="/write">
-          <Write />
-          </Route>
-
-
-          <Route  exact path="/register">
-            {user ? <HomePage /> : <Register />}
-          </Route>
-
-          <Route  exact path="/login">
-          {user ? <HomePage /> : <Login />}
-          </Route>
-
-          <Route  path="/post/:postId">
-            <Single />
-          </Route>
-
-         
-
-          <Route exact path="/settings">
-          {user ? <Settings /> : <Register />} 
-          </Route>
-
-
-          
-
-
-          </Switch>   
-   
+    <Router>
+      <Topbar />
+      <Switch>
+        <Route exact path="/">
+          <Homepage />
+        </Route>
+        <Route path="/posts">
+          <Homepage />
+        </Route>
+        <Route path="/register">
+          {currentUser ? <Homepage /> : <Register />}
+        </Route>
+        <Route path="/login">{currentUser ? <Homepage /> : <Login />}</Route>
+        <Route path="/post/:id">
+          <Single />
+        </Route>
+        <Route path="/write">{currentUser ? <Write /> : <Login />}</Route>
+        <Route path="/settings">
+          {currentUser ? <Settings /> : <Login />}
+        </Route>
+      </Switch>
     </Router>
   );
-  }
+}
 
 export default App;
