ddf64af30dbce0a9857d4ca7749d4afb104135a4
Elliott Arnold <si3mshady@Elliotts-MBP.attlocal.net>
2021-08-07 18:05:02 -0500

    finished backend apis for blogsite

--- backend/models/Category.js
+++ backend/models/Category.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 // create category schema 
 
-const CategorySchema = new mongoose.Schema(({
+const CategorySchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
